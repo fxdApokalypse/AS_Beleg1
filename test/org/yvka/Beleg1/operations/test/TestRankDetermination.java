d183edbbfaa14ae354d51aee1f6c95c5b5ab883f
Yves Kaufmann <fxdapokalypse@googlemail.com>
2014-05-10 14:06:08 +0200

    Commenting the the test case for the bug InvalidRankDetermination

--- test/org/yvka/Beleg1/operations/test/TestRankDetermination.java
+++ test/org/yvka/Beleg1/operations/test/TestRankDetermination.java
@@ -1,25 +1,27 @@
-/**
- * 
- */
 package org.yvka.Beleg1.operations.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.yvka.Beleg1.data.Matrix;
 import org.yvka.Beleg1.data.MatrixFactory;
 import org.yvka.Beleg1.operations.CommonMatrixOperations;
 
 /**
+ * TestCases which make sure that the rank determination functions correctly. 
+ * 
  * @author Yves Kaufmann
  *
  */
 public class TestRankDetermination {
 	
 	protected static MatrixFactory factory = MatrixFactory.get();
-		
+	
+	/**
+	 * Tests the determination of the rank for identity matrices
+	 */
 	@Test
+	@SuppressWarnings("static-method")
 	public void testIdentityMatrix() {
 		Matrix identyMatrix3x3 = factory.createMatrixFromArray(new double [][] {
 				{1.0, 0.0, 0.0},
@@ -37,8 +39,12 @@ public class TestRankDetermination {
 		assertEquals("The rank of quadtratic identity matrix must be rank(M) = rows(M)", 1, 
 				CommonMatrixOperations.determineRankOfMatrix(identyMatrix1x1));
 	}
-
+	
+	/**
+	 * Tests the determination of the rank for non quadratic matrices
+	 */
 	@Test
+	@SuppressWarnings("static-method")
 	public void testNonQuadtraticMatrices() {
 		Matrix matrix2x6 = factory.createMatrixFromArray(new double [][] {
 				{2.0, 2.0, 6.0, 0.0, 4.0, 12.0},
@@ -59,11 +65,6 @@ public class TestRankDetermination {
 		
 		assertEquals("The rank of a matrix must be rank(M) = min(rows(M),cols(M)) - NonZeroRows(Gauss(M)) ", 1, 
 				CommonMatrixOperations.determineRankOfMatrix(matrix3x1));
-		
-		
-		
+			
 	}
-	
-	
-
 }
